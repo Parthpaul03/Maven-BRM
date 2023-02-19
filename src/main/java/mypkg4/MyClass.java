97f3fb189125bf475f19689be1e0c0a7c0dbef15
Parth Paul <parthpaul03@gmail.com>
2023-02-19 15:47:29 +0530

    Second commit

--- src/main/java/mypkg4/MyClass.java
+++ src/main/java/mypkg4/MyClass.java
@@ -5,7 +5,10 @@ public class MyClass {
 	{
 		System.out.println("Welcome to jenkins");
 	}
-
+	public void display()
+	{
+		System.out.println("Hello World ");
+	}
 	public static void main(String[] args) {
 		MyClass ob= new MyClass();
 		ob.show();
